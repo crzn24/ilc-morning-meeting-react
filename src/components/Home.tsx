24c8675dadc81f15ee4282a7376f98438b5987e8
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-10-23 21:07:38 -0700

    Installs tailwind config

---
+++ src/components/Home.tsx
@@ -0,0 +1,10 @@
+// run "rafce" to create a functional component using the ES7+ React plugin 
+// import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
