b62f6d32faabf89b997e8523aecf8edcbe2ae214
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-11-01 20:51:47 -0700

    Creates a button to return to current date

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,21 @@ import { format } from "date-fns";
 const App = () => {
   const [currentDate, setCurrentDate] = useState(new Date());
 
+  const handleSetToday = () => setCurrentDate(new Date());
+
   return (
-    <div className="mt-16 flex flex-col items-center">
-      <div>
-        <p>Selected Date: {format(currentDate,'dd LLLL yyyy')}</p>
+    <div className="mt-16 flex flex-col items-center gap-8">
+      <div className="flex flex-col items-center gap-4">
+        <p>Selected Date: {format(currentDate, "dd LLLL yyyy")}</p>
+
+        <button
+          className="text-sm px-4 py-1 rounded text-white bg-blue-600 hover:bg-blue-700 active:bg-blue-800"
+          onClick={handleSetToday}
+        >
+          Today
+        </button>
       </div>
+
       <Calendar value={currentDate} onChange={setCurrentDate} />
     </div>
   );
